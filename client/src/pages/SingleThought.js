795fb15aa85f2fdcae1bb51e21d23b9e418d9a7f
Eric <ericyoung111@gmail.com>
2022-02-03 11:29:55 -0700

    made the single thought, friend list and profile pages

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
+  //get the thought id from the url parameter
+  const { id: thoughtId } = useParams();
+  //query the thought data using the thoughtId passed into useQuery hook as a variable
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            Username
+            {thought.username}'s
           </span>{' '}
-          thought on createdAt
+          thought on {thought.createdAt}
         </p>
         <div className="card-body">
-          <p>Thought Text</p>
+          <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
