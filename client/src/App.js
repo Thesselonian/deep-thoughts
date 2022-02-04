ea1b2d0841c153f091a19cdf038fbaba61839cfd
Eric <ericyoung111@gmail.com>
2022-02-03 17:22:58 -0700

    made login and sign up features that use front end authentication by JWT. Page conditionally renders based on logged in or not

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
+import Home from './pages/Home';
 import Signup from './pages/Signup';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-import Home from './pages/Home';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
