a6825b663ef53f292d45f8459274ab988be8304c
Eric <ericyoung111@gmail.com>
2022-02-05 07:43:01 -0700

    made the reaction form and the thought form

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import Auth from '../utils/auth';
 
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = props => {
   //get the thought id from the url parameter
@@ -24,7 +26,7 @@ const SingleThought = props => {
       <div className="card mb-3">
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
-            {thought.username}'s
+            {thought.username}
           </span>{' '}
           thought on {thought.createdAt}
         </p>
@@ -34,6 +36,7 @@ const SingleThought = props => {
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
